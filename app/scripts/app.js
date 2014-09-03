fe8403ead1de2f63b81be8e9f1c8d19f7d07c35a
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-09-03 09:52:40 +0200

    Bootstrap with yo

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name xkeFrontApp
+ * @description
+ * # xkeFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('xkeFrontApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xkeFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xkeFrontApp
+ */
+angular.module('xkeFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
