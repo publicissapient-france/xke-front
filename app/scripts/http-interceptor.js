534130ad931863b2fe985f5f3b3e2e886a834d70
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-09-03 16:04:36 +0200

    Use preprod backend url if front url is on github

--- app/scripts/http-interceptor.js
+++ app/scripts/http-interceptor.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('xke').config(['$httpProvider', function ($httpProvider) {
+  var backendUrl = 'http://localhost:8080';
+
+  if (/xebia-france\\.github\\.io/.test(window.location.hostname)) {
+    backendUrl = 'http://xke-test.xebiafr.eu.cloudbees.net'; // prod url for API
+  }
 
     $httpProvider.interceptors.push([function () {
         return {
             'request': function(config) {
                 if (config.url.indexOf('/api') === 0) {
-                    config.url = 'http://xke-test.xebiafr.eu.cloudbees.net' + config.url;
+                    config.url = backendUrl + config.url;
                 }
 
                 return config;
