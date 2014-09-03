dece59cc25ac7aadd05a683852599f77ac13899a
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-09-03 10:23:34 +0200

    Migrate xke application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name xkeFrontApp
- * @description
- * # xkeFrontApp
- *
- * Main module of the application.
- */
-angular
-  .module('xkeFrontApp', []);
+angular.module('xke', ['ui.select2'])
+  .config(function ($routeProvider, $httpProvider) {
+    $httpProvider.defaults.headers.common = {
+      'X-Requested-With': 'XMLHttpRequest',
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'};
+    $routeProvider
+      .when('/', { templateUrl: 'views/slot/list.html', controller: 'ListSlotCtrl' })
+      .when('/new', { templateUrl: 'views/slot/detail.html', controller: 'CreateSlotCtrl' })
+      .when('/edit/:slotId', { templateUrl: 'views/slot/detail.html', controller: 'EditSlotCtrl' })
+      .when('/idea/new', { templateUrl: 'views/idea/detail.html', controller: 'CreateSlotCtrl' })
+      .when('/idea/edit/:slotId', { templateUrl: 'views/idea/detail.html', controller: 'EditSlotCtrl' })
+      .when('/users', { templateUrl: 'views/users/list.html', controller: 'ListUsersCtrl' })
+      .when('/users/:userId', { templateUrl: 'views/users/show.html', controller: 'ShowUsersCtrl' })
+      .otherwise({ redirectTo: '/' });
+  });
---
+++ app/scripts/controllers.js
@@ -0,0 +1,156 @@
+'use strict';
+
+var types = ["Contest", "Demo", "Debate", "Dojo", "Formal", "Hackathon", "Hands-on", "Other", "Quickie", "Training"];
+var durations = [15, 30, 45, 60, 90, 120, 150, 180, 210, 240];
+var fondations = ["Craft", "Mobile", "Agile", "Front", "Back", "Data", "Cloud", "DevOps", "Divers"];
+
+angular.module('xke')
+  .controller('ListSlotCtrl', function ($scope, $http, $location) {
+
+  $scope.slotsJustPlayed = [];
+  $scope.xkeDate = new Date();
+
+  $http.get('/slot')
+    .success(function (data) {
+      $scope.slots = data;
+    })
+  ;
+
+  $scope.edit = function (slot) {
+    $location.path('/edit/' + slot.id);
+  }
+
+  $scope.takeIdea = function (idea) {
+    var position = $scope.slots.ideas.indexOf(idea);
+    idea.speakers.push($scope.username);
+    $http.put('/slot/' + idea.id, idea)
+      .success(function (slot) {
+        $scope.slots.ideas.splice(position, 1);
+        $scope.slots.readySlots.push(slot);
+      })
+    ;
+  }
+
+  $scope.addOrRemoveSlotToPlayedArray = function(slot) {
+    var slotPosition = $scope.slotsJustPlayed.indexOf(slot);
+    if (slotPosition == -1) {
+      $scope.slotsJustPlayed.push(slot);
+    } else {
+      $scope.slotsJustPlayed.splice(slotPosition, 1);
+    }
+  }
+
+  $scope.playedAt = function() {
+    $scope.slotsJustPlayed.forEach(function (slot) {
+      slot.playedDates.push($scope.xkeDate);
+      $http.put('/slot/' + slot.id, slot);
+    });
+    // Message d'erreur et de succès ?
+    $scope.slotsJustPlayed = [];
+    $scope.xkeDate = new Date();
+  }
+
+})
+  .controller('CreateSlotCtrl', function ($scope, $location, $http) {
+  $scope.saving = false;
+
+  $scope.types = types;
+  $scope.durations = durations;
+  $scope.fondations = fondations;
+
+  $scope.slot = {type: types[0], fondation: fondations[0], duration: durations[0], pitch: null};
+
+  $http.get('/user')
+    .success(function (data) {
+      $scope.speakers = data;
+    })
+  ;
+
+  $scope.save = function () {
+    $scope.slot.creator = $scope.username;
+    if ($scope.slot.speakers == null) {
+      $scope.slot.speakers = new Array();
+    }
+    $scope.slot.playedDates = new Array();
+    $scope.saving = true;
+    // TODO 1 : appeler un service qui va enregistrer le slot
+    $http.post('/slot', $scope.slot)
+      .success(function () {
+        $scope.saving = false;
+        $location.path('/');
+      })
+      .error(function () {
+        $scope.saving = false;
+      })
+    ;
+  };
+})
+  .controller('EditSlotCtrl', function ($scope, $location, $routeParams, $http) {
+  var self = this;
+
+  $scope.types = types;
+  $scope.durations = durations;
+  $scope.fondations = fondations;
+
+  $scope.slotId = $routeParams.slotId;
+
+  $http.get('/user')
+    .success(function (data) {
+      $scope.speakers = data;
+    })
+  ;
+
+  $http.get('/slot/' + $routeParams.slotId)
+    .success(function (data) {
+      $scope.slot = data;
+    })
+  ;
+
+  $scope.isClean = function () {
+    return angular.equals(self.original, $scope.slot);
+  }
+
+  $scope.destroy = function () {
+    // TODO 1 : appeler un service qui va supprimer le slot
+    $http.delete('/slot/' + $routeParams.slotId)
+      .success(function () {
+        $location.path('/');
+      })
+    ;
+  };
+
+  $scope.transformIntoIdea = function() {
+    $scope.slot.speakers = [];
+    $http.put('/slot/' + $scope.slot.id, $scope.slot)
+      .success(function () {
+        $location.path('/');
+      })
+    ;
+  };
+
+  $scope.save = function () {
+    // TODO 1 : appeler un service qui va mettre-à-jour le slot
+    $scope.slot.title = $scope.slot.title.replace(/"/g, "\\\"");
+    $http.put('/slot/' + $scope.slot.id, $scope.slot)
+      .success(function () {
+        $location.path('/');
+      })
+    ;
+  };
+})
+  .controller('ListUsersCtrl', function ($scope, $http) {
+
+  $http.get('/user')
+    .success(function (data) {
+      $scope.users = data;
+    })
+  ;
+})
+  .controller('ShowUsersCtrl', function ($scope, $http, $routeParams) {
+
+  $http.get('/user/' + $routeParams.userId)
+    .success(function (data) {
+      $scope.userInfo = data;
+    })
+  ;
+});
